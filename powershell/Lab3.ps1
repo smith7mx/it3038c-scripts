4fa414e2f53ce21c06b32762d5a12df9a368afa9
smith7mx <smith7mx@mail.uc.edu>
2022-09-14 16:12:30 -0400

    'Lab3'

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,22 @@
+﻿function getIP{
+(Get-NetIPAddress).ipv4address | Select-String "192*"
+}
+$IP=getIP
+function getAdmin{
+whoami
+}
+$User=getAdmin
+function getHost{
+hostname
+}
+$Host1=getHost
+function getVersion{
+$PSVersionTable.PSVersion.Major
+}
+$Version=(getVersion)
+function getDate{
+Get-Date
+}
+$Date=(getDate)
+$BODY=("This machine's IP is $IP. User is $User. Hostname is $Host1. Powershell Version $Version. Today's Date is $Date.")
+$BODY | Out-File C:\it3038c-scripts\powershell\Lab3Script.txt
