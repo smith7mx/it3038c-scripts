7b963d459ea0c92a0bb878888b7e160b1d3b4743
smith7mx <smith7mx@mail.uc.edu>
2022-10-28 14:01:17 -0400

    Project2

--- project1/Project1.py
+++ project1/Project1.py
@@ -1,5 +1,8 @@
 from datetime import datetime
+import os
 
 time=datetime.now()
 current=time.strftime("%H:%M:%S")
 print("The time is:", current)
+print("Current signed in User: " + os.getlogin())
+print("Current Working Directory: " + os.getcwd())
