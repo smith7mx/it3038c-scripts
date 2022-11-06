151050b47fe2b328bf98941772ae4db1e1aa447b
smith7mx <smith7mx@mail.uc.edu>
2022-11-06 13:14:31 -0500

    test

---
+++ node/api.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const data = require("C:/Users/Administrator/Downloads/widgets.json");
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+      res.writeHead(200, {"Content-Type": "text/json"});
+      res.end(JSON.stringify(data));
+    }
+    else {
+      res.writeHead(404, {"Content-Type": "text/plain"});
+      res.end("Data not found");
+    }
+  });
+
+server.listen(3000);
+console.log("Server is listening on port 3000");
